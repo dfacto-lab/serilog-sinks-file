2197a31dace433ec99b811df52cdf8fc9001c96a
Eamon Hetherton <eamonhetherton@gmail.com>
2016-11-11 12:42:49 +1000

    Added tests to demonstrate issue with appending to file sink when using an encoding with preamble and a file size limit

---
+++ test/Serilog.Sinks.File.Tests/FixedOutputFormatter.cs
@@ -0,0 +1,25 @@
+﻿using Serilog.Formatting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Serilog.Events;
+using System.IO;
+
+namespace Serilog.Tests
+{
+    public class FixedOutputFormatter : ITextFormatter
+    {
+        string _substitutionText;
+
+        public FixedOutputFormatter(string substitutionText)
+        {
+            _substitutionText = substitutionText;
+        }
+
+        public void Format(LogEvent logEvent, TextWriter output)
+        {
+            output.Write(_substitutionText);
+        }
+    }
+}
